f6c0209c7f7d8c9e02995fa3faee68b528c457df
Sreesha <sreeshan90@gmail.com>
2016-04-07 22:16:07 -0500

    Added files via upload

---
+++ src/com/misc/problems/CTCT_1_5.java
@@ -0,0 +1,43 @@
+package com.google.interview;
+
+public class CTCT_1_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String input = "aabcccccaaaa";
+		if(compress(input).length()<input.length()){
+			System.out.println(compress(input));
+		}
+		else{
+			System.out.println(input);
+		}
+	}
+
+	public static String compress(String a) {
+
+		int[] letters = new int[256];
+		StringBuilder retValue = new StringBuilder();
+		// count the no of occurances of each char in the string
+
+		for (int i = 0; i < a.length(); i++) {
+			letters[(int) a.charAt(i)]++;
+
+		}
+
+		for (int i = 0; i < letters.length; i++) {
+			
+			if(letters[i] > 0){
+				retValue.append((char)i);
+				retValue.append(letters[i]);
+				//letters[i] = -1;
+			}
+		
+
+		}
+		
+		return retValue.toString();
+
+	}
+	
+}
