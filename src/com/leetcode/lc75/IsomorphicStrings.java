fde4f9b859f53ac36b066f45f1af1993d84ef9a5
Sreesha Nagaraj <sreeshanagaraj@Sreeshas-MacBook-Pro.local>
2022-12-26 00:10:20 -0800

    LC73 day 2

---
+++ src/com/leetcode/lc75/IsomorphicStrings.java
@@ -0,0 +1,34 @@
+package com.leetcode.lc75;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Character> map = new HashMap<>();
+
+        for (int i=0; i<s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                if (map.get(s.charAt(i)) == t.charAt(i)) {
+                    continue;
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                if(map.containsValue(t.charAt(i))){
+                    return false;
+                }
+                map.put(s.charAt(i), t.charAt(i));
+            }
+        }
+
+        return true;
+    }
+}
